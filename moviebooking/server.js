68ded0e1f570346e9a46511edf42f42eba36dfb5
SilentSaint <rakshithg35@gmail.com>
2021-07-16 11:56:33 +0530

    checkpoint 5

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -1,5 +1,22 @@
+//core imports
 const http = require("http");
+//local imports
+const db = require("./models");
 const PORT = 9000;
+
+//db connection
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 const ROUTE_RESPONSE_MAP = {
   "/movies": "All Movies Data in JSON format from Mongo DB",
